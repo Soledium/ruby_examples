14b8460b720debfae40e2d6dbf9e7e58ca0255f6
Soledium <nescobar3@hotmail.com>
2022-05-26 12:44:59 -0400

    Agregando ejemplos de  variables en ruby

---
+++ variable.rb
@@ -0,0 +1,13 @@
+#sumando dos variables
+x=10
+y=20
+suma = x + y
+puts suma
+precio_dolar= 827
+peso_cl= 500
+
+conversion=precio_dolar * peso_cl
+puts conversion
+texto_saludo="Hola desde Chile"
+puts texto_saludo
+puts "Hasta la próxima"
