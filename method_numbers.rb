e4f4f8cea947e565c9fb97b480020a488fbb0f54
Soledium <nescobar3@hotmail.com>
2022-05-27 09:35:35 -0400

    Agregando metodos de numeros en ruby

---
+++ method_numbers.rb
@@ -0,0 +1,10 @@
+
+numero_entero=1776
+#metodo para numeros enteros siguiente o succesor
+puts numero_entero.next
+puts numero_siguiente = numero_entero.succ
+#metodo para validar si un numero es par o impar
+puts numero_entero.odd?
+puts numero_entero.even?
+puts numero_siguiente.odd?
+puts numero_siguiente.even?
