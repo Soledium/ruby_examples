669e572d60f8a21764438fcf5d83ddaf2ad105c8
Soledium <nescobar3@hotmail.com>
2022-05-27 10:07:59 -0400

    Agregando archivo de métodos en números

--- method_numbers.rb
+++ method_numbers.rb
@@ -8,3 +8,14 @@ puts numero_entero.odd?
 puts numero_entero.even?
 puts numero_siguiente.odd?
 puts numero_siguiente.even?
+#conversion de numero entero a flotante
+puts numero_siguiente / numero_entero.to_f
+puts 800 / 270.to_f
+#longitud de un numero
+print 356.digits
+print 78905.digits
+#redondear un numero flotante
+puts 5609.78203.round
+puts 5609.78203.to_i
+puts 5609.78203.floor
+puts 5609.78203.ceil
