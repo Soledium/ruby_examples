7e5b408ef68b5bd8f7e616d44b5f1f604aa60b78
Soledium <nescobar3@hotmail.com>
2022-06-06 12:39:28 -0400

    agregando ejercicios ARGV

---
+++ examples_argv.rb
@@ -0,0 +1,7 @@
+primero = ARGV[0].to_i
+segundo = ARGV[1].to_i
+tercero = ARGV[2].to_i
+suma = primero + segundo + tercero
+#puts primero + segundo + tercero 
+puts "La variable de argv #{primero} + #{segundo} + {tercero} y la suma #{primero + segundo + tercero}"
+puts ARGV.class 
