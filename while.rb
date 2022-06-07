6e94652fc280104824b4ebd9a3e10fcd6e94968d
Soledium <nescobar3@hotmail.com>
2022-06-07 12:00:22 -0400

    agregando ejercicio de while

---
+++ while.rb
@@ -0,0 +1,14 @@
+i = 5
+while  i > 0
+puts "Conteo: #{i}"
+i -= 1
+end
+puts "Despegue"
+
+j = 0
+suma = 0
+while j < 10
+    suma += j
+    j += 1
+end
+puts "La suma de los primeros 10 numeros es #{suma}"
