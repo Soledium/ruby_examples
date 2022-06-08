7c25e24d947a79fe21ae153a948d9f8334cfd3db
Soledium <nescobar3@hotmail.com>
2022-06-08 13:58:35 -0400

    ejemplo modificado

--- numero_intercalados.rb
+++ numero_intercalados.rb
@@ -3,18 +3,18 @@ print "Ingresa cuantos numeros deseas imprimir: " #p sirve para imprimir pero te
 n = gets.chomp.to_i
 
 (1..n).each do |var_block|
-#     if var_block.odd?       #odd-->impar | even-->par
-#         print var_block
-#     else
-#         print "."     
-#     end
-# end    
-
-    if var_block%2 == 0   #el modulo que te entrega el resto de la division
+    if var_block.odd?       #odd-->impar | even-->par
         print var_block
     else
-        print "."
-end
+        print "."     
+    end
+end    
+
+#     if var_block%2 == 0   #el modulo que te entrega el resto de la division
+#         print var_block
+#     else
+#         print "."
+# end
 
 
 
