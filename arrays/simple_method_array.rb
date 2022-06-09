26497349f5ab601742dfbf6a20180794a5afd972
Soledium <nescobar3@hotmail.com>
2022-06-09 11:42:09 -0400

    ejercicios

---
+++ arrays/simple_method_array.rb
@@ -0,0 +1,25 @@
+#declarando arreglos
+arregloInt = [4923,32,745,3232,89,2,1,898,888,2]
+arregloString = ['Miguel', 'Paula', 'Nath', 'MJ', 'Juana', 'César']
+#operaciones con arreglos
+print arregloInt.size
+puts ""
+print arregloString.length
+puts ""
+print arregloInt.reverse
+puts ""
+print arregloString.reverse
+puts ""
+print arregloInt.shuffle
+puts ""
+print arregloString.shuffle
+puts ""
+puts arregloInt.include?(777)
+puts arregloString.include?('Nath')
+arregloString.delete('Miguel')
+arregloString.delete_at(2)
+print arregloString
+
+
+
+
