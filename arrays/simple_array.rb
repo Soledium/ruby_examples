f9b0814948e7f222a84a8382e7dfce9f82d77155
Soledium <nescobar3@hotmail.com>
2022-06-09 10:10:27 -0400

    ejercicio array simple

---
+++ arrays/simple_array.rb
@@ -0,0 +1,15 @@
+
+a = [5,6,4,9,12,1,0]
+print a
+puts ""
+puts a[0]
+puts a[5]
+puts a[-1]          #siempre me traerá el último número 
+print a[2,4]        #el 2 representa desde donde me debe traer el número (desde adelante) y el cuatro la cantidad
+print a[-2,3]       #el -2 representa que debe posicionarse traerme desde el segundo en adelante desde los ultimos numeros y el 3 la cantidad
+puts "#{a[0]}  #{a[5]}"
+b = ['Arica', 'Iquique', 'Antofagasta']
+print b
+puts ""
+puts b[1]
+puts b[-3]
