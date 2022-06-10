444471ff08ac8e06ef51faddc2a3b2d377379dad
Soledium <nescobar3@hotmail.com>
2022-06-10 12:54:59 -0400

    ejercicio visitas

---
+++ Pruebas miscelaneas/visitas.rb
@@ -0,0 +1,9 @@
+visitas = [1000,800,250,300,500,2500]
+
+def promedio(arreglo)
+    promedio = arreglo.sum/arreglo.length   #.count o .size hacen lo mismo pero length es transversal a los lenguages de programación
+end
+
+puts promedio(visitas)
+
+
